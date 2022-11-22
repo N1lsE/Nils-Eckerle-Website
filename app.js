93b9ec337276c34d36b9879dbcacd5fa1ab9ca7c
N1lsE <nilseckerle2001@gmail.com>
2022-11-22 20:26:52 +0100

    improved navbar for mobile devices. used code from this tutorial:
    https://www.youtube.com/watch?v=3-2Pj5hxwrw

---
+++ app.js
@@ -0,0 +1,25 @@
+// navbar toggle code was coppied from https://www.youtube.com/watch?v=3-2Pj5hxwrw
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbarMenu");
+const navLogo = document.querySelector("#navbarLogo");
+
+// display mobile menu
+const mobileMenu = () => {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.toggle("active");
+};
+
+menu.addEventListener("click", mobileMenu);
+
+// Close Mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector(".is-active");
+    if (window.innerWidth <= 820 && menuBars) {
+      menu.classList.toggle("is-active");
+      menuLinks.classList.remove("active");
+    }
+  };
+  
+  menuLinks.addEventListener("click", hideMobileMenu);
+  navLogo.addEventListener("click", hideMobileMenu);
+// End of stealing code
